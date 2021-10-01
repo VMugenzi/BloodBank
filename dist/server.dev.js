88f3a40c983ec6c3dd41cf7b59c924d8d162cf4d
VMugenzi <84989141+VMugenzi@users.noreply.github.com>
2021-10-01 13:07:59 +0100

    ft-server

---
+++ dist/server.dev.js
@@ -0,0 +1,37 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
+var _express = _interopRequireDefault(require("express"));
+
+var _mongoose = _interopRequireDefault(require("mongoose"));
+
+var _dotenv = _interopRequireDefault(require("dotenv"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+_dotenv["default"].config({
+  path: './.env'
+});
+
+var app = (0, _express["default"])();
+var databaseUrl = process.env.DATABASE;
+
+_mongoose["default"].connect(databaseUrl, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false
+}).then(function () {
+  return console.log("Database connected sucessfully");
+});
+
+app.listen(2021, function () {
+  console.log('Server is running on port 2021');
+});
+var _default = app;
+exports["default"] = _default;
+//# sourceMappingURL=server.dev.js.map
